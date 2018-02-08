7fc53dd9c995797d84b6c927dbe2771fc8158907
RiseToDev <ram14apple@gmail.com>
2018-02-08 19:44:16 +0200

    Fixed the round mathod that is led to the crash of the app

--- app/src/main/java/rise_mike/finance/DataCollaboration.java
+++ app/src/main/java/rise_mike/finance/DataCollaboration.java
@@ -102,16 +102,18 @@ public final class DataCollaboration {
      * To round the values of the rate of each currency
      */
     public String getRound(String value) {
-        String[] numberSplit = value.split("\\.");
-        Double buf = Double.parseDouble(numberSplit[1]);
-        if (buf == 0) {
-            return numberSplit[0];
-        } else {
-            buf = Double.parseDouble(value);
-            DecimalFormat df = new DecimalFormat("#.###");
-            buf = Double.valueOf(df.format(buf));
-            return buf.toString();
-        }
+        if (value.contains(".")) {
+            String[] numberSplit = value.split("\\.");
+            Double buf = Double.parseDouble(numberSplit[1]);
+            if (buf == 0) {
+                return numberSplit[0];
+            } else {
+                buf = Double.parseDouble(value);
+                DecimalFormat df = new DecimalFormat("#.###");
+                buf = Double.valueOf(df.format(buf));
+                return buf.toString();
+            }
+        } else return value;
     }
 }
 
