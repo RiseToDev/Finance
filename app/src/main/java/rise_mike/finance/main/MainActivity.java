6d0cab712ea4f9dc1e1b9de3e05f6e208dc8b7a8
MikeInCode <serbin.michael98@gmail.com>
2018-02-12 19:35:41 +0200

    Recreated content layout

--- app/src/main/java/rise_mike/finance/main/MainActivity.java
+++ app/src/main/java/rise_mike/finance/main/MainActivity.java
@@ -1,6 +1,7 @@
 package rise_mike.finance.main;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,6 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
@@ -32,7 +36,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar_content);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -46,8 +50,8 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(this, Rates.class);
             startActivity(intent);
         });
-    }
 
+    }
 
     @Override
     public void onBackPressed() {
